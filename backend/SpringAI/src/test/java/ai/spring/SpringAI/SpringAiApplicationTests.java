d0a6f4f24e94111569166fd18f9f8551368ccbf1
Sadab Halim <sadabhalim.sh@gmail.com>
2025-01-10 13:39:14 +0530

    Add backend code implementation

---
+++ backend/SpringAI/src/test/java/ai/spring/SpringAI/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.spring.SpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
